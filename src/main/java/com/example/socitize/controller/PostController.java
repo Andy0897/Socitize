91c46b21bdb567f1cad1f9bfb93c8a07e6da5ec3
PC01-204 <andiiidim0@gmail.com>
2024-04-19 09:30:23 +0300

    Commit

---
+++ src/main/java/com/example/socitize/controller/PostController.java
@@ -0,0 +1,12 @@
+package com.example.socitize.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/post")
+public class PostController {
+    @GetMapping("/create")
+    public String
+}
--- src/main/java/com/example/socitize/entity/Post.java
+++ src/main/java/com/example/socitize/entity/Post.java
@@ -1,7 +1,50 @@
 package com.example.socitize.entity;
 
-import jakarta.persistence.Entity;
+import jakarta.annotation.Nullable;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Max;
 
 @Entity
 public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    @Max(value = 500, message = "The text must contain less than 500 symbols")
+    private String text;
+    @Lob
+    private byte[] image;
+    @ManyToOne
+    private User user;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public byte[] getImage() {
+        return image;
+    }
+
+    public void setImage(byte[] image) {
+        this.image = image;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
 }
